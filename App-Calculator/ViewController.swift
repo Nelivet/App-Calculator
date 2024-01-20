ed080aeeb929a9f31cfe732fdf7ced4a0be350ce
Ivette Fernández <ivettefernandezrodriguez@gmail.com>
2024-01-20 00:34:52 -0500

    second Commit about constrains , IBAction and IBOutlet.

--- App-Calculator/ViewController.swift
+++ App-Calculator/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var calculatorOperations: UILabel!
+   
+    @IBOutlet weak var calculatorResults: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // this is
-        
-        
     }
+    
+    
+    @IBAction func deleteAllTap(_ sender: Any) {
+    }
+    
+    @IBAction func backTap(_ sender: Any) {
+    }
+    
+    @IBAction func percentageTap(_ sender: Any) {
+    }
+    
+    @IBAction func divideTap(_ sender: Any) {
+    }
+    @IBAction func multiplierTap(_ sender: Any) {
+    }
+    @IBAction func subtractionTap(_ sender: Any) {
+    }
+    @IBAction func additionTap(_ sender: Any) {
+    }
+    @IBAction func equalsTap(_ sender: Any) {
+    }
+    
+    
+    @IBAction func zeroTap(_ sender: Any) {
+    }
+    
+    @IBAction func decimalTap(_ sender: Any) {
+    }
+    
+    @IBAction func nunOneTap(_ sender: Any) {
+    }
+    @IBAction func nunTwoTap(_ sender: Any) {
+    }
+    @IBAction func nunThreeTap(_ sender: Any) {
+    }
+    @IBAction func nunFourTap(_ sender: Any) {
+    }
+    @IBAction func nunFiveTap(_ sender: Any) {
+    }
+    @IBAction func nunSixTap(_ sender: Any) {
+    }
+    
+    @IBAction func nunSevenTap(_ sender: Any) {
+    }
+    
+    @IBAction func nunEightTap(_ sender: Any) {
+    }
+    @IBAction func nunNineTap(_ sender: Any) {
+    }
+    
 }
 
